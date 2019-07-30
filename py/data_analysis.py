298ee5139632d6797f502eb635baac81f918c80a
Thomas James Delaney <thomas.delaney@posteo.net>
2019-07-30 18:27:25 +0100

    Adding parallelisation. Must commit to test.

--- py/data_analysis.py
+++ py/data_analysis.py
@@ -5,8 +5,8 @@ import pandas as pd
 import datetime as dt
 from scipy.stats import pearsonr
 from itertools import combinations
-if float(sys.version[:3])<3.0:
-    from pyentropy import DiscreteSystem
+from multiprocessing import Pool, cpu_count
+from pyitlib import discrete_random_variable as drv
 
 bin_widths = np.array([0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0]) # various bin widths for testing measurement values.
 
@@ -23,6 +23,16 @@ def getBinsForSpikeCounts(spike_time_dict, bin_width, spon_start_time):
     end_time = spon_start_time + num_bins * bin_width
     return np.arange(spon_start_time, end_time + bin_width, bin_width)
 
+def getCellSpikeCountBinFrame(cell_id, spike_time_dict, spike_count_bins):
+    """
+    Helper function for getSpikeCountBinFrame.
+    """
+    num_rows = spike_count_bins.size - 1
+    spike_counts = np.histogram(spike_time_dict[cell_id], bins=spike_count_bins)[0]
+    bin_starts = spike_count_bins[:-1]
+    bin_stops = spike_count_bins[1:]
+    return pd.DataFrame({'cell_id':np.repeat(cell_id, num_rows), 'spike_count':spike_counts, 'bin_start_time':bin_starts, 'bin_stop_time':bin_stops})
+
 def getSpikeCountBinFrame(cell_ids, spike_time_dict, spike_count_bins):
     """
     For getting a frame showing the spike counts with bin times.
@@ -31,13 +41,25 @@ def getSpikeCountBinFrame(cell_ids, spike_time_dict, spike_count_bins):
                 spike_count_bins, numpy.array (float), the bin boundaries
     Returns:    DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
     """
-    def getCellSpikeCountBinFrame(cell_id, spike_time_dict, spike_count_bins):
-        num_rows = spike_count_bins.size - 1
-        spike_counts = np.histogram(spike_time_dict[cell_id], bins=spike_count_bins)[0]
-        bin_starts = spike_count_bins[:-1]
-        bin_stops = spike_count_bins[1:]
-        return pd.DataFrame({'cell_id':np.repeat(cell_id, num_rows), 'spike_count':spike_counts, 'bin_start_time':bin_starts, 'bin_stop_time':bin_stops})
-    
+    cell_ids = np.array([cell_ids]) if np.isscalar(cell_ids) else cell_ids
+    num_workers = cpu_count()
+    chunk_size = int(cell_ids.size/num_workers)
+    pool = Pool(processes = num_workers)
+    cell_spike_count_bin_frame_futures = pool.starmap_async(getCellSpikeCountBinFrame, zip(cell_ids, [spike_time_dict] * cell_ids.size, [spike_count_bins] * cell_ids.size))
+    cell_spike_count_bin_frame_futures.wait()
+    pool.close(); pool.join();
+    return pd.concat(cell_spike_count_bin_frame_futures.get(), ignore_index=True)
+
+def getSpikeCountBinFrameOld(cell_ids, spike_time_dict, spike_count_bins):
+    """
+    For getting a frame showing the spike counts with bin times.
+    Arguments:  cell_ids, numpy.array (int), the cell ids
+                spike_time_dict, dict, cell_id => spike_times
+                spike_count_bins, numpy.array (float), the bin boundaries
+    Returns:    DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
+
+    DEPRECATED
+    """
     cell_ids = np.array([cell_ids]) if np.isscalar(cell_ids) else cell_ids
     return pd.concat([getCellSpikeCountBinFrame(cell_id, spike_time_dict, spike_count_bins) for cell_id in cell_ids], ignore_index=True)
 
@@ -65,29 +87,22 @@ def getMutualInfoForPair(pair, spike_count_frame):
                 spike_count_frame, DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
     Returns:    plugin_mi, float, plugin mutual information
                 plugin_shuff_mi, float, plugin shuffled mutual information
-                qe_mi, float, quadratic extrapolation corrected mutual information
-                qe_shuff_mi, quadratic extrapolation corrected mutual information
     """
     first_spike_counts = spike_count_frame.loc[spike_count_frame.cell_id == pair[0], 'spike_count']
     second_spike_counts = spike_count_frame.loc[spike_count_frame.cell_id == pair[1], 'spike_count']
-    first_response_dims = [1, 1 + first_spike_counts.max()]
-    second_response_dims = [1, 1 + second_spike_counts.max()]
-    discrete_system = DiscreteSystem(first_spike_counts, first_response_dims, second_spike_counts, second_response_dims)
-    discrete_system.calculate_entropies(method='plugin', calc=['HX', 'HXY', 'HiXY', 'HshXY'])
-    plugin_mi = np.max([0, discrete_system.I()])
-    plugin_shuff_mi = np.max([discrete_system.Ish()])
-    discrete_system.calculate_entropies(method='qe', calc=['HX', 'HXY'])
-    discrete_system.calculate_entropies(method='qe', calc=['HX', 'HXY', 'HiXY', 'HshXY'])
-    qe_mi = np.max([0, discrete_system.I()])
-    qe_shuff_mi = np.max([discrete_system.Ish()])
-    return plugin_mi, plugin_shuff_mi, qe_mi, qe_shuff_mi
+    first_response_alphabet = np.arange(first_spike_counts.max()+1)
+    second_response_alphabet = np.arange(second_spike_counts.max()+1)
+    plugin_mi = np.max([0, drv.information_mutual(X=first_spike_counts, Y=second_spike_counts, Alphabet_X=first_response_alphabet, Alphabet_Y=second_response_alphabet)])
+    np.random.shuffle(second_spike_counts)
+    plugin_shuff_mi = np.max([0, drv.information_mutual(X=first_spike_counts, Y=second_spike_counts, Alphabet_X=first_response_alphabet, Alphabet_Y=second_response_alphabet)])
+    return plugin_mi, plugin_shuff_mi
 
 def getAnalysisFrameForCells(cell_ids, spike_count_frame):
     """
     For measuring the correlation coefficients and mutual information between the cells, using the counts in the spike_count_frame.
     Arguments:  cell_ids, numpy.array (int), the cell ids
                 spike_count_frame, DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
-    Returns:    analysis_frame, DataFrame, corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, qe_mi, qe_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv
+    Returns:    analysis_frame, DataFrame, corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv
     """
     pairs = np.array(list(combinations(cell_ids, 2)))
     num_pairs = pairs.shape[0]
@@ -97,12 +112,10 @@ def getAnalysisFrameForCells(cell_ids, spike_count_frame):
     shuff_corr_pv = np.zeros(num_pairs)
     plugin_mi = np.zeros(num_pairs)
     plugin_shuff_mi = np.zeros(num_pairs)
-    qe_mi = np.zeros(num_pairs)
-    qe_shuff_mi = np.zeros(num_pairs)
     for i,pair in enumerate(pairs):
         corr_coef[i], corr_pv[i], shuff_corr[i], shuff_corr_pv[i] = getSpikeCountCorrelationsForPair(pair, spike_count_frame)
         plugin_mi[i], plugin_shuff_mi[i], qe_mi[i], qe_shuff_mi[i] = getMutualInfoForPair(pair, spike_count_frame)
-    analysis_frame = pd.DataFrame({'first_cell_id':pairs[:,0], 'second_cell_id':pairs[:,1], 'corr_coef':corr_coef, 'corr_pv':corr_pv, 'shuff_corr':shuff_corr, 'shuff_corr_pv':shuff_corr_pv, 'plugin_mi':plugin_mi, 'plugin_shuff_mi':plugin_shuff_mi, 'qe_mi':qe_mi, 'qe_shuff_mi':qe_shuff_mi})
+    analysis_frame = pd.DataFrame({'first_cell_id':pairs[:,0], 'second_cell_id':pairs[:,1], 'corr_coef':corr_coef, 'corr_pv':corr_pv, 'shuff_corr':shuff_corr, 'shuff_corr_pv':shuff_corr_pv, 'plugin_mi':plugin_mi, 'plugin_shuff_mi':plugin_shuff_mi})
     return analysis_frame
 
 def getAllBinsFrameForCells(cell_ids, spike_time_dict, spon_start_time):
@@ -111,10 +124,10 @@ def getAllBinsFrameForCells(cell_ids, spike_time_dict, spon_start_time):
     Arguments:  cell_ids, numpy.array (int), the cell ids
                 spike_time_dict, dict, cell_id => spike_times
                 spon_start_time, the time at which spontaneous behaviour begins
-    Returns:    DataFrame, corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, qe_mi, qe_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv, bin_width
+    Returns:    DataFrame, corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv, bin_width
     """
     analysis_frames = []
-    for i,bin_width, in enumerate(bin_widths):
+    for i,bin_width in enumerate(bin_widths):
         print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing bin width ' + str(bin_width) + '...')
         spike_count_bins = getBinsForSpikeCounts(spike_time_dict, bin_width, spon_start_time)
         spike_count_frame = getSpikeCountBinFrame(cell_ids, spike_time_dict, spike_count_bins)
--- py/data_loading.py
+++ py/data_loading.py
@@ -30,6 +30,31 @@ def loadVideoDataForMouse(mouse_name, mat_dir):
     face_file_name = mouse_name + '_face_proc.mat'
     return loadmat(os.path.join(mat_dir, face_file_name))
 
+def getSpikesForCell(cell_id, cell_info, mouse_spikes, spon_start_time):
+    specific_cell_info = cell_info.loc[cell_id]
+    probe_spikes = mouse_spikes[specific_cell_info['probe_id']]
+    cell_spike_times = probe_spikes['st'].flatten()[probe_spikes['clu'].flatten() == specific_cell_info['mouse_probe_cell_id']]
+    return cell_spike_times[cell_spike_times >= spon_start_time]
+
+def loadSpikeTimeDictOld(mouse_name, cell_ids, cell_info, mat_dir):
+    """
+    For loading a dictionary of cell_id => spike times.
+    Arguments:  mouse_name, string, the name of the mouse
+                cell_ids, the cell ids for which we want the spike times
+                cell_info, pandas.DataFrame, a table of information on the cells, gives the probe id, and the mouse_probe_cell_id
+                mat_dir, the directory where the spikes file can be found
+    Returns:    spike_dict, dictionary, cell_id => spike times
+
+    DEPRECATED: New loadSpikeTimeDict is parallelised and is therefore slightly faster, or perhaps much faster.
+    """
+    cell_ids = np.array([cell_ids]) if np.isscalar(cell_ids) else cell_ids
+    spon_start_time = spon_start_times[np.flatnonzero(mouse_names == mouse_name)[0]]
+    mouse_spikes = loadSpikesForMouse(mouse_name, mat_dir)
+    relevant_cell_info = cell_info.loc[cell_ids]
+    required_probes = relevant_cell_info.probe_id.unique()
+    spike_times_for_cells = [getSpikesForCell(cell_id, cell_info, mouse_spikes, spon_start_time) for cell_id in cell_ids]
+    return {cell_id:spike_times for (cell_id, spike_times) in zip(cell_ids, spike_times_for_cells)}
+
 def loadSpikeTimeDict(mouse_name, cell_ids, cell_info, mat_dir):
     """
     For loading a dictionary of cell_id => spike times.
@@ -40,35 +65,6 @@ def loadSpikeTimeDict(mouse_name, cell_ids, cell_info, mat_dir):
     Returns:    spike_dict, dictionary, cell_id => spike times
     """
     cell_ids = np.array([cell_ids]) if np.isscalar(cell_ids) else cell_ids
-    spike_dict = {}
-    spon_start_time = spon_start_times[np.flatnonzero(mouse_names == mouse_name)[0]]
-    mouse_spikes = loadSpikesForMouse(mouse_name, mat_dir)
-    relevant_cell_info = cell_info.loc[cell_ids]
-    required_probes = relevant_cell_info.probe_id.unique()
-    for cell_id in cell_ids:
-        specific_cell_info = cell_info.loc[cell_id]
-        probe_spikes = mouse_spikes[specific_cell_info['probe_id']]
-        cell_spike_times = probe_spikes['st'].flatten()[probe_spikes['clu'].flatten() == specific_cell_info['mouse_probe_cell_id']]
-        spike_dict[cell_id] = cell_spike_times[cell_spike_times >= spon_start_time]
-    return spike_dict
-
-def getSpikesForCell(cell_id, cell_info, mouse_spikes, spon_start_time):
-    specific_cell_info = cell_info.loc[cell_id]
-    probe_spikes = mouse_spikes[specific_cell_info['probe_id']]
-    cell_spike_times = probe_spikes['st'].flatten()[probe_spikes['clu'].flatten() == specific_cell_info['mouse_probe_cell_id']]
-    return cell_spike_times[cell_spike_times >= spon_start_time]
-
-def loadSpikeTimeDictParallel(mouse_name, cell_ids, cell_info, mat_dir):
-    """
-    For loading a dictionary of cell_id => spike times.
-    Arguments:  mouse_name, string, the name of the mouse
-                cell_ids, the cell ids for which we want the spike times
-                cell_info, pandas.DataFrame, a table of information on the cells, gives the probe id, and the mouse_probe_cell_id
-                mat_dir, the directory where the spikes file can be found
-    Returns:    spike_dict, dictionary, cell_id => spike times
-    """
-    cell_ids = np.array([cell_ids]) if np.isscalar(cell_ids) else cell_ids
-    spike_dict = {}  
     spon_start_time = spon_start_times[np.flatnonzero(mouse_names == mouse_name)[0]]
     mouse_spikes = loadSpikesForMouse(mouse_name, mat_dir)
     relevant_cell_info = cell_info.loc[cell_ids]
@@ -76,6 +72,7 @@ def loadSpikeTimeDictParallel(mouse_name, cell_ids, cell_info, mat_dir):
     num_workers = cpu_count()
     chunk_size = int(cell_ids.size/num_workers)
     pool = Pool(processes = num_workers)
-    spike_times_for_cells_futures = pool.starmap_async(getSpikesForCell, zip(cell_ids, [cell_info] * cell_ids.size, [mouse_spikes] * cell_ids.size, [spon_start_time] * cell_ids.size))
+    spike_times_for_cells_futures = pool.starmap_async(getSpikesForCell, zip(cell_ids, [cell_info] * cell_ids.size, [mouse_spikes] * cell_ids.size, [spon_start_time] * cell_ids.size), chunksize=chunk_size)
     spike_times_for_cells_futures.wait()
+    pool.close(); pool.join();
     return {cell_id:spike_times for (cell_id, spike_times) in zip(cell_ids, spike_times_for_cells_futures.get())}
