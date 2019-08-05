2b53ba88fd41425a3c7de16bb546f3d7b45033b5
Thomas James Delaney <thomas.delaney@posteo.net>
2019-08-05 18:16:19 +0100

    Trying to make scoop work. Currently won't work on my laptop, trying on Blue crystal anyway.

--- sh/blue_crystal_submission_script.sh
+++ sh/blue_crystal_submission_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-$PBS_O_WORKDIR=$HOME/Eight_Probe
+$PBS_O_WORKDIR=Eight_Probe
 
 # request resources:
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=4:ppn=16
 #PBS -l walltime=02:00:00
 # on compute node, change directory to 'submission directory':
 cd $PBS_O_WORKDIR
 # run your program, timing it for good measure:
-time $HOME/Eight_Probe/py/bin_width_variation.py
+time python -m scoop $HOME/Eight_Probe/py/bin_width_variation.py
