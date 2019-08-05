500267e3f7f8f2873976e3b79cff9cebe5fff27c
Thomas James Delaney <thomas.delaney@posteo.net>
2019-08-05 23:17:45 +0100

    Added some finishing touches to bin_width_variation scripts. Updated submission script.

--- py/bin_width_variation.py
+++ py/bin_width_variation.py
@@ -4,7 +4,7 @@ For varying the bin width used from 0.005 to 2 seconds, and taking measurements 
 We save a frame for spike counts for each time bin containing all cells. We also save a frame for each time bin for all pairs.
 """
 import os, argparse, sys
-if float(sys.version[:3])<3.0:
+if float(sys.version[:3]) < 3.0:
     execfile(os.path.join(os.environ['HOME'], '.pystartup'))
 import numpy as np
 import pandas as pd
@@ -13,7 +13,7 @@ from itertools import product, combinations
 from scoop import futures
 
 parser = argparse.ArgumentParser(description='For varying the bin width used from 0.005 to 4 seconds, and taking measurements using these bin widths.')
-parser.add_argument('-s', '--save_file_suffix', help='A suffix for the name of the file in which to save the measurement frame.', type=str, default='_all_bins.npy')
+parser.add_argument('-n', '--number_of_cells', help='Number of cells to process. Use 0 for all.', type=int, default=10)
 parser.add_argument('-d', '--debug', help='Enter debug mode.', default=False, action='store_true')
 args = parser.parse_args()
 
@@ -33,10 +33,15 @@ def constructMapFuncArgs(pairs, spike_count_frame):
     """
     For constructing a list of dictionaries to be passed into a mapping function for scoop.futures.mapReduce. 
     In this context the mapping function can only take one argument.
+    Arguments:  pairs, numpy.array, all the possible pairs.
+                spike_count_frame, DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time 
     """
     return [{'pair':pair, 'spike_count_frame':scf} for pair, scf in zip(pairs, [spike_count_frame] * pairs.shape[0])]
 
 def saveSpikeCountFrame(cell_ids, bin_width, spike_time_dict, spon_start_time, mouse_name):
+    """
+    For getting, saving, and returning the spike count frame, with the bin width column. Also returns the name of the file where the spike_count_bins was saved.
+    """
     spike_count_bins = ep.getBinsForSpikeCounts(spike_time_dict, bin_width, spon_start_time)
     spike_count_frame = ep.getSpikeCountBinFrame(cell_ids, spike_time_dict, spike_count_bins)
     spike_count_frame['bin_width'] = bin_width
@@ -47,7 +52,7 @@ def saveSpikeCountFrame(cell_ids, bin_width, spike_time_dict, spon_start_time, m
 def getAnalysisDictForPair(arg_dict):
     """
     For getting a dictionary containing measurements for the given pair. This function is most useful for parallel processing
-    as there will be a great number of pairs.
+    as there will be a great number of pairs. This is the mapping function for mapReduce.
     Arguments:  arg_dict['pair'], numpy.array (int, 2), cell ids of the pair
                 arg_dict['spike_count_frame'], spike_count_frame, DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
     Returns:    Dict,   keys=(corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv)
@@ -57,23 +62,22 @@ def getAnalysisDictForPair(arg_dict):
     return {'corr_coef': np.repeat(corr,1), 'corr_pv':np.repeat(corr_pv,1), 'first_cell_id':np.repeat(arg_dict['pair'][0],1), 'plugin_mi':np.repeat(plugin_mi,1), 'plugin_shuff_mi':np.repeat(plugin_shuff_mi,1), 'second_cell_id':np.repeat(arg_dict['pair'][1],1), 'shuff_corr':np.repeat(shuff_corr,1), 'shuff_corr_pv':np.repeat(shuff_corr_pv,1)}
 
 def reduceAnalysisDicts(first_dict, second_dict):
+    """
+    Each dict is str => numpy array, and will have the same keys. This function appends the dictionaries together. This is the reduce function for mapReduce.
+    Arguments:  first_dict, keys=(corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv)
+                second_dict, keys=(corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv)
+    Return:     Dict, keys=(corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv)
+    """
     return {k:np.append(first_dict[k], second_dict[k]) for k in first_dict.keys()}
 
-def saveAnalysisFrame(pairs, spike_count_frame, bin_width, mouse_name):
-    analysis_frame = futures.mapReduce(getAnalysisFrameForPair, reduceAnalysisFrames, zip(pairs, [spike_count_frame] * pairs.shape[0]))
-    analysis_frame['bin_width'] = bin_width
-    save_file = os.path.join(npy_dir, 'analysis_frames', mouse_name + '_' + str(bin_width).replace('.', 'p') + '_' + 'analysis.npy')
-    analysis_frame.to_pickle(save_file)
-    return save_file
-
-# def main():
 if (not args.debug) & (__name__ == "__main__"):
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Loading cell info...')
     cell_info = pd.read_csv(os.path.join(csv_dir, 'cell_info.csv'), index_col=0)
     for m,mouse_name in enumerate(ep.mouse_names):
         print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing mouse ' + mouse_name + '...')
         spon_start_time = ep.spon_start_times[m]
-        cell_ids = cell_info[cell_info.mouse_name == mouse_name].index.values[:30] # using 10 cells for testing
+        cell_ids = cell_info[cell_info.mouse_name == mouse_name].index.values
+        cell_ids = cell_ids[:args.number_of_cells] if args.number_of_cells > 0 else cell_ids # selecting fewer cells for testing
         spike_time_dict = ep.loadSpikeTimeDict(mouse_name, cell_ids, cell_info, mat_dir)
         pairs = np.array(list(combinations(cell_ids, 2)))
         for bin_width in ep.bin_widths:
@@ -83,8 +87,5 @@ if (not args.debug) & (__name__ == "__main__"):
             analysis_frame['bin_width'] = bin_width
             save_file = os.path.join(npy_dir, 'analysis_frames', mouse_name + '_' + str(bin_width).replace('.', 'p') + '_' + 'analysis.npy')
             analysis_frame.to_pickle(save_file)
-            # analysis_save_file = saveAnalysisFrame(pairs, spike_count_frame, bin_width, mouse_name) 
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Done.')
 
-# if not(args.debug):
-#     main()
--- sh/blue_crystal_submission_script.sh
+++ sh/blue_crystal_submission_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-$PBS_O_WORKDIR=Eight_Probe
+PBS_O_WORKDIR=Eight_Probe
 
 # request resources:
 #PBS -l nodes=4:ppn=16
-#PBS -l walltime=02:00:00
+#PBS -l walltime=04:00:00
 # on compute node, change directory to 'submission directory':
 cd $PBS_O_WORKDIR
 # run your program, timing it for good measure:
