31f23d5617a93b293cf8b2ecfa3e37d1783aab0c
Thomas James Delaney <thomas.delaney@posteo.net>
2019-08-05 18:14:30 +0100

    Trying to make scoop work. Currently won't work on my laptop, trying on Blue crystal anyway.

--- py/bin_width_variation.py
+++ py/bin_width_variation.py
@@ -29,6 +29,13 @@ mat_dir = os.path.join(proj_dir, 'mat')
 sys.path.append(os.environ['PROJ'])
 import Eight_Probe.py as ep
 
+def constructMapFuncArgs(pairs, spike_count_frame):
+    """
+    For constructing a list of dictionaries to be passed into a mapping function for scoop.futures.mapReduce. 
+    In this context the mapping function can only take one argument.
+    """
+    return [{'pair':pair, 'spike_count_frame':scf} for pair, scf in zip(pairs, [spike_count_frame] * pairs.shape[0])]
+
 def saveSpikeCountFrame(cell_ids, bin_width, spike_time_dict, spon_start_time, mouse_name):
     spike_count_bins = ep.getBinsForSpikeCounts(spike_time_dict, bin_width, spon_start_time)
     spike_count_frame = ep.getSpikeCountBinFrame(cell_ids, spike_time_dict, spike_count_bins)
@@ -37,20 +44,20 @@ def saveSpikeCountFrame(cell_ids, bin_width, spike_time_dict, spon_start_time, m
     spike_count_frame.to_pickle(save_file)
     return save_file, spike_count_frame
 
-def getAnalysisFrameForPair(pair, spike_count_frame):
+def getAnalysisDictForPair(arg_dict):
     """
     For getting a dictionary containing measurements for the given pair. This function is most useful for parallel processing
     as there will be a great number of pairs.
-    Arguments:  pair, numpy.array (int, 2), cell ids of the pair
-                spike_count_frame, spike_count_frame, DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
+    Arguments:  arg_dict['pair'], numpy.array (int, 2), cell ids of the pair
+                arg_dict['spike_count_frame'], spike_count_frame, DataFrame, cell_id, spike_count, bin_start_time, bin_stop_time
     Returns:    Dict,   keys=(corr_coef, corr_pv, first_cell_id, plugin_mi, plugin_shuff_mi, second_cell_id, shuff_corr, shuff_corr_pv)
     """
-    corr, corr_pv, shuff_corr, shuff_corr_pv = ep.getSpikeCountCorrelationsForPair(pair, spike_count_frame)
-    plugin_mi, plugin_shuff_mi = ep.getMutualInfoForPair(pair, spike_count_frame)
-    return pd.DataFrame({'corr_coef': corr, 'corr_pv':corr_pv, 'first_cell_id':pair[0], 'plugin_mi':plugin_mi, 'plugin_shuff_mi':plugin_shuff_mi, 'second_cell_id':pair[1], 'shuff_corr':shuff_corr, 'shuff_corr_pv':shuff_corr_pv}, index=[0])
+    corr, corr_pv, shuff_corr, shuff_corr_pv = ep.getSpikeCountCorrelationsForPair(arg_dict['pair'], arg_dict['spike_count_frame'])
+    plugin_mi, plugin_shuff_mi = ep.getMutualInfoForPair(arg_dict['pair'], arg_dict['spike_count_frame'])
+    return {'corr_coef': np.repeat(corr,1), 'corr_pv':np.repeat(corr_pv,1), 'first_cell_id':np.repeat(arg_dict['pair'][0],1), 'plugin_mi':np.repeat(plugin_mi,1), 'plugin_shuff_mi':np.repeat(plugin_shuff_mi,1), 'second_cell_id':np.repeat(arg_dict['pair'][1],1), 'shuff_corr':np.repeat(shuff_corr,1), 'shuff_corr_pv':np.repeat(shuff_corr_pv,1)}
 
-def reduceAnalysisFrames(first_frame, second_frame):
-    return pd.concat([first_frame, second_frame], ignore_index=True)
+def reduceAnalysisDicts(first_dict, second_dict):
+    return {k:np.append(first_dict[k], second_dict[k]) for k in first_dict.keys()}
 
 def saveAnalysisFrame(pairs, spike_count_frame, bin_width, mouse_name):
     analysis_frame = futures.mapReduce(getAnalysisFrameForPair, reduceAnalysisFrames, zip(pairs, [spike_count_frame] * pairs.shape[0]))
@@ -60,7 +67,7 @@ def saveAnalysisFrame(pairs, spike_count_frame, bin_width, mouse_name):
     return save_file
 
 # def main():
-if __name__ == "__main__":
+if (not args.debug) & (__name__ == "__main__"):
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Loading cell info...')
     cell_info = pd.read_csv(os.path.join(csv_dir, 'cell_info.csv'), index_col=0)
     for m,mouse_name in enumerate(ep.mouse_names):
@@ -72,7 +79,7 @@ if __name__ == "__main__":
         for bin_width in ep.bin_widths:
             print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing bin width ' + str(bin_width) + '...')
             spike_count_save_file, spike_count_frame = saveSpikeCountFrame(cell_ids, bin_width, spike_time_dict, spon_start_time, mouse_name)
-            analysis_frame = futures.mapReduce(getAnalysisFrameForPair, reduceAnalysisFrames, list(zip(pairs, [spike_count_frame] * pairs.shape[0])))
+            analysis_frame = pd.DataFrame.from_dict(futures.mapReduce(getAnalysisDictForPair, reduceAnalysisDicts, constructMapFuncArgs(pairs, spike_count_frame)))
             analysis_frame['bin_width'] = bin_width
             save_file = os.path.join(npy_dir, 'analysis_frames', mouse_name + '_' + str(bin_width).replace('.', 'p') + '_' + 'analysis.npy')
             analysis_frame.to_pickle(save_file)
