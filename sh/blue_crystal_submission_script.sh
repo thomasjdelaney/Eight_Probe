2c8c825a204bcf518b98a26d1ba9a9445218eff2
Thomas James Delaney <thomas.delaney@posteo.net>
2019-08-06 00:35:59 +0100

    Edited submission script.

--- sh/blue_crystal_submission_script.sh
+++ sh/blue_crystal_submission_script.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-PBS_O_WORKDIR=Eight_Probe
-
+#
 # request resources:
-#PBS -l nodes=4:ppn=16
-#PBS -l walltime=04:00:00
+#PBS -l nodes=4:ppn=16,walltime=01:00:00
+
 # on compute node, change directory to 'submission directory':
+export WORK_DIR=Eight_Probe
+
 cd $PBS_O_WORKDIR
+cat $PBS_NODEFILE
+export nodes=`cat $PBS_NODEFILE`
+export nnodes=`cat $PBS_NODEFILE | wc -l`
+export confile=inf.$PBS_JOBID.conf
+
+for i in $nodes
+do
+  echo ${i}>>$confile
+done
+
 # run your program, timing it for good measure:
-time python -m scoop $HOME/Eight_Probe/py/bin_width_variation.py
+time python -m scoop --hostfile $confile -n $nnodes $HOME/Eight_Probe/py/bin_width_variation.py -n 0
