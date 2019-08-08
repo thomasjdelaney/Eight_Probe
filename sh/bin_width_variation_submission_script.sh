99bf06c57898e9b24e15b012af1c07ec38035eb7
Thomas Delaney <thomas.delaney@posteo.net>
2019-08-08 17:37:13 +0100

    Wrote final draft of bin_width_variation_submission_script.

--- sh/bin_width_variation_submission_script.sh
+++ sh/bin_width_variation_submission_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # request resources:
-#PBS -l nodes=4:ppn=16,walltime=01:00:00
+#PBS -l nodes=4:ppn=16,walltime=04:00:00
 
 # on compute node, change directory to 'submission directory':
 
@@ -15,4 +15,6 @@ do
 done
 
 # run your program, timing it for good measure:
-time python -m scoop --hostfile $confile -n $nnodes $HOME/Eight_Probe/py/bin_width_variation.py -n 0
+# NB: Only using 50 cells here because of memory constraints
+time python -m scoop --hostfile $confile -n $nnodes $HOME/Eight_Probe/py/bin_width_variation.py -n 50
+rm confile
