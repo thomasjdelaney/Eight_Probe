9a08051f6600b9d722d9fd69d2efd92a7f7c7c8e
Thomas Delaney <thomas.delaney@posteo.net>
2019-10-11 17:28:07 +0100

    Added some code to the measurement analysis.

--- py/measurement_distribution_analysis.py
+++ py/measurement_distribution_analysis.py
@@ -35,9 +35,20 @@ def plotMeasureHistogram(analysis_frame, measurement, x_label, y_label, x_lims=N
     plt.title(title, fontsize='x-large') if title != '' else None
     plt.xticks(fontsize='large'); plt.yticks(fontsize='large')
 
-m=0
+cell_info = pd.read_csv(os.path.join(csv_dir, 'cell_info.csv'), index_col=0)
+m=1
 mouse_name = ep.mouse_names[m]
 analysis_frame = ep.loadAnalysisFrame(mouse_name, args.bin_width, csv_dir)
+analysis_frame = analysis_frame.join(cell_info['cell_region'], how='left', on='first_cell_id')
+analysis_frame = analysis_frame.rename(columns={'cell_region':'first_cell_region'})
+analysis_frame = analysis_frame.join(cell_info['cell_region'], how='left', on='second_cell_id')
+analysis_frame = analysis_frame.rename(columns={'cell_region':'second_cell_region'})
+data_regions = analysis_frame.first_cell_region.unique().values
+measure_regional_aggregation = pd.DataFrame()
+for region_pair in combinations(data_regions, 2):
+    region_pair_analysis_frame = analysis_frame.loc[(analysis_frame.first_cell_region == region_pair[0]) & (analysis_frame.second_cell_region == region_pair[1]) |
+        (analysis_frame.first_cell_region == region_pair[1]) & (analysis_frame.second_cell_region == region_pair[0])]
+    measure_regional_aggregation.append({'first_region':region_pair[0], 'second_region':region_pair[1], 'num_pairs':region_pair_analysis_frame.shape[0], 'mean_corr':region_pair_analysis_frame.corr_coef.mean(), 'std_corr':region_pair_analysis_frame.corr_coef.std(), 'mean_shuff_corr':region_pair_analysis_frame.shuff_corr.mean(), 'std_shuff_corr':region_pair_analysis_frame.shuff_corr.std(), 'mean_info':region_pair_analysis_frame.bias_corrected_mi.mean(), 'std_info':region_pair_analysis_frame.bias_corrected_mi.mstd()})
 
 plotMeasureHistogram(analysis_frame, 'corr_coef', 'Corr. Coef.', 'Num. Occurances', x_lims=(-1,1), title='Num pairs = ' + str(analysis_frame.shape[0]))
 file_name = os.path.join(image_dir, 'correlation_histograms','test_corr_hist.png')
