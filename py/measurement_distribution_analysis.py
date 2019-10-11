a00b5f6ed59c19dcdba1ccda1b80c234f08502cd
Thomas James Delaney <thomas.delaney@posteo.net>
2019-10-11 23:51:53 +0100

    Added a lot of images. Also added a lot to measurement_distribution_analysis.py

--- py/measurement_distribution_analysis.py
+++ py/measurement_distribution_analysis.py
@@ -27,7 +27,43 @@ mat_dir = os.path.join(proj_dir, 'mat')
 sys.path.append(os.environ['PROJ'])
 import Eight_Probe.py as ep
 
+def joinCellAnalysis(cell_info, analysis_frame):
+    analysis_frame = analysis_frame.join(cell_info['cell_region'], how='left', on='first_cell_id')
+    analysis_frame = analysis_frame.rename(columns={'cell_region':'first_cell_region'})
+    analysis_frame = analysis_frame.join(cell_info['cell_region'], how='left', on='second_cell_id')
+    analysis_frame = analysis_frame.rename(columns={'cell_region':'second_cell_region'})
+    return analysis_frame 
+
+def getFullRegionPairList(analysis_frame):
+    data_regions = analysis_frame.first_cell_region.unique()
+    comb_list = list(combinations(data_regions, 2))
+    return comb_list + [(r,r) for r in data_regions]
+
+def getRegionalAnalysisFrame(analysis_frame, region_pair):
+    return analysis_frame.loc[(analysis_frame.first_cell_region == region_pair[0]) & (analysis_frame.second_cell_region == region_pair[1]) | (analysis_frame.first_cell_region == region_pair[1]) & (analysis_frame.second_cell_region == region_pair[0])]
+
+def getAnalysisAggregationDict(region_pair_analysis_frame):
+    return {'first_region':region_pair[0], 'second_region':region_pair[1], 'num_pairs':region_pair_analysis_frame.shape[0], 'mean_corr':region_pair_analysis_frame.corr_coef.mean(), 'std_corr':region_pair_analysis_frame.corr_coef.std(), 'mean_shuff_corr':region_pair_analysis_frame.shuff_corr.mean(), 'std_shuff_corr':region_pair_analysis_frame.shuff_corr.std(), 'mean_info':region_pair_analysis_frame.bias_corrected_mi.mean(), 'std_info':region_pair_analysis_frame.bias_corrected_mi.std()}
+
+def getPlotTitle(mouse_name, num_pairs, region_pair):
+    title = 'Mouse=' + mouse_name 
+    if region_pair[0] == region_pair[1]:
+        title = title + ', Region=' + region_pair[0]
+    else:
+        title = title + ', Regions=(' + region_pair[0] + ',' + region_pair[1] + ')' 
+    return title + ', Num pairs=' + str(num_pairs)
+
+def getFileName(subdir, mouse_name, bin_width, region_pair, suffix):
+    base_file_name = mouse_name + '_' + str(bin_width).replace('.', 'p') 
+    if region_pair[0] == region_pair[1]:
+        base_file_name = base_file_name + '_' + region_pair[0]
+    else:
+        base_file_name = base_file_name + '_' + region_pair[0] + '_' + region_pair[1] 
+    base_file_name = base_file_name + suffix + '.png'
+    return os.path.join(image_dir, subdir, base_file_name)
+
 def plotMeasureHistogram(analysis_frame, measurement, x_label, y_label, x_lims=None, title=''):
+    plt.figure(figsize=(6,5))
     plt.hist(analysis_frame[measurement], bins=50)
     plt.xlim(x_lims) if x_lims != None else None
     plt.xlabel(x_label, fontsize='x-large')
@@ -35,23 +71,30 @@ def plotMeasureHistogram(analysis_frame, measurement, x_label, y_label, x_lims=N
     plt.title(title, fontsize='x-large') if title != '' else None
     plt.xticks(fontsize='large'); plt.yticks(fontsize='large')
 
-cell_info = pd.read_csv(os.path.join(csv_dir, 'cell_info.csv'), index_col=0)
-m=1
-mouse_name = ep.mouse_names[m]
-analysis_frame = ep.loadAnalysisFrame(mouse_name, args.bin_width, csv_dir)
-analysis_frame = analysis_frame.join(cell_info['cell_region'], how='left', on='first_cell_id')
-analysis_frame = analysis_frame.rename(columns={'cell_region':'first_cell_region'})
-analysis_frame = analysis_frame.join(cell_info['cell_region'], how='left', on='second_cell_id')
-analysis_frame = analysis_frame.rename(columns={'cell_region':'second_cell_region'})
-data_regions = analysis_frame.first_cell_region.unique().values
-measure_regional_aggregation = pd.DataFrame()
-for region_pair in combinations(data_regions, 2):
-    region_pair_analysis_frame = analysis_frame.loc[(analysis_frame.first_cell_region == region_pair[0]) & (analysis_frame.second_cell_region == region_pair[1]) |
-        (analysis_frame.first_cell_region == region_pair[1]) & (analysis_frame.second_cell_region == region_pair[0])]
-    measure_regional_aggregation.append({'first_region':region_pair[0], 'second_region':region_pair[1], 'num_pairs':region_pair_analysis_frame.shape[0], 'mean_corr':region_pair_analysis_frame.corr_coef.mean(), 'std_corr':region_pair_analysis_frame.corr_coef.std(), 'mean_shuff_corr':region_pair_analysis_frame.shuff_corr.mean(), 'std_shuff_corr':region_pair_analysis_frame.shuff_corr.std(), 'mean_info':region_pair_analysis_frame.bias_corrected_mi.mean(), 'std_info':region_pair_analysis_frame.bias_corrected_mi.mstd()})
+def plotHistogramsSave(region_pair_analysis_frame, region_pair, mouse_name, bin_width):
+    plot_title = getPlotTitle(mouse_name, region_pair_analysis_frame.shape[0], region_pair)
+    plotMeasureHistogram(region_pair_analysis_frame, 'corr_coef', 'Corr. Coef.', 'Num. Occurances', x_lims=(-1,1), title=plot_title)
+    plt.savefig(getFileName('correlation_histograms', mouse_name, args.bin_width, region_pair, '_corr_hist')); plt.close('all')
+    plotMeasureHistogram(region_pair_analysis_frame, 'shuff_corr', 'Corr. Coef. (Shuffled)', 'Num. Occurances', x_lims=(-1,1), title=plot_title)
+    plt.savefig(getFileName('shuffled_correlation_histograms', mouse_name, args.bin_width, region_pair, '_shuff_hist')); plt.close('all')
+    plotMeasureHistogram(region_pair_analysis_frame, 'bias_corrected_mi', 'MI (bits)', 'Num. Occurances', title=plot_title)
+    plt.savefig(getFileName('information_histograms', mouse_name, args.bin_width, region_pair, '_info_hist')); plt.close('all')
 
-plotMeasureHistogram(analysis_frame, 'corr_coef', 'Corr. Coef.', 'Num. Occurances', x_lims=(-1,1), title='Num pairs = ' + str(analysis_frame.shape[0]))
-file_name = os.path.join(image_dir, 'correlation_histograms','test_corr_hist.png')
-plt.savefig(file_name)
-# need to add regions 
-
+if (not args.debug) & (__name__ == '__main__'):
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Loading cell info...')
+    measure_statistics = pd.DataFrame()
+    cell_info = pd.read_csv(os.path.join(csv_dir, 'cell_info.csv'), index_col=0)
+    for m,mouse_name in enumerate(ep.mouse_names):
+        print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing mouse ' + mouse_name + '...')
+        analysis_frame = ep.loadAnalysisFrame(mouse_name, args.bin_width, csv_dir)
+        analysis_frame = joinCellAnalysis(cell_info, analysis_frame)
+        mouse_regional_aggregation = pd.DataFrame()
+        for region_pair in getFullRegionPairList(analysis_frame):
+            region_pair_analysis_frame = getRegionalAnalysisFrame(analysis_frame, region_pair)
+            mouse_regional_aggregation = mouse_regional_aggregation.append(getAnalysisAggregationDict(region_pair_analysis_frame), ignore_index=True)
+            plotHistogramsSave(region_pair_analysis_frame, region_pair, mouse_name, args.bin_width)
+        mouse_regional_aggregation['mouse_name'] = mouse_name
+        measure_statistics = measure_statistics.append(mouse_regional_aggregation, ignore_index=True)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Saving measure statistics...')
+    measure_statistics.to_csv(os.path.join(csv_dir, 'measure_statistics.csv'))
+print(dt.datetime.now().isoformat() + ' INFO: ' + 'Done.')
