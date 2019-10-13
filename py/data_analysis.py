d69be92fa0dac5827774ebb36c22f42dbf67764f
Thomas James Delaney <thomas.delaney@posteo.net>
2019-10-13 20:21:43 +0100

    Added 0.1 to the selected bin widths. Just for fun.

--- py/data_analysis.py
+++ py/data_analysis.py
@@ -11,7 +11,7 @@ from functools import reduce
 
 bin_widths = np.array([0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0]) # various bin widths for testing measurement values.
 augmented_bin_widths = np.concatenate([[0.001, 0.002, 0.005], bin_widths])
-selected_bin_widths = np.array([1.0, 2.0, 3.0])
+selected_bin_widths = np.array([0.1, 1.0, 2.0, 3.0])
 
 def getRegionallyDistributedCells(cell_info, num_cells):
     """
