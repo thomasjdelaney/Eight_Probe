c624a92c86779e22a1d7ff6dc14d42b816ed58a2
Thomas James Delaney <thomas.delaney@posteo.net>
2020-04-02 20:41:05 +0100

    Edited the detect communities submission script.

--- sh/detect_communities_submission_script.sh
+++ sh/detect_communities_submission_script.sh
@@ -16,4 +16,25 @@ done
 
 # run your program, timing it for good measure:
 # NB: Only using 50 cells here because of memory constraints
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r total -c rectified
+echo `date +'%d-%m-%Y %T'`" INFO: Completed total rectified."
 time python3 $HOME/Eight_Probe/py/detect_communities.py -r conditional -c rectified
+echo `date +'%d-%m-%Y %T'`" INFO: Completed conditional rectified."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r signal -c rectified
+echo `date +'%d-%m-%Y %T'`" INFO: Completed signal rectified."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r total -c negative
+echo `date +'%d-%m-%Y %T'`" INFO: Completed total negative."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r conditional -c negative
+echo `date +'%d-%m-%Y %T'`" INFO: Completed conditional negtive."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r signal -c negative
+echo `date +'%d-%m-%Y %T'`" INFO: Completed signal negative."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r total -c absolute
+echo `date +'%d-%m-%Y %T'`" INFO: Completed total absolute."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r conditional -c absolute
+echo `date +'%d-%m-%Y %T'`" INFO: Completed conditional absolute."
+time python3 $HOME/Eight_Probe/py/detect_communities.py -r signal -c absolute
+echo `date +'%d-%m-%Y %T'`" INFO: Completed signal absolute."
+echo `date +'%d-%m-%Y %T'`" INFO: Done."
+
+
+
