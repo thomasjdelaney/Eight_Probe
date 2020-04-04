8cc5725c68a41225dbd57eaa7b621f7c9997116b
Thomas James Delaney <thomas.delaney@posteo.net>
2020-04-04 20:59:12 +0100

    Edited cluster comparison script.

--- py/cluster_comparison.py
+++ py/cluster_comparison.py
@@ -16,6 +16,7 @@ from sklearn.metrics import mutual_info_score, adjusted_mutual_info_score, norma
 from math import isclose
 
 parser = argparse.ArgumentParser(description='For comparing clusterings using the variation of information matric (VI)')
+parser.add_argument('-r', '--correlation_type', help='Correlation type. Either "total", "conditional", "signal".', default='total', choices=['total', 'conditional', 'signal'], type=str)
 parser.add_argument('-c', '--correction', help='Correction type. Either "absolute", "rectified", or "negative".', default='rectified', choices=['rectified', 'negative', 'absolute'], type=str)
 parser.add_argument('-d', '--debug', help='Enter debug mode.', default=False, action='store_true')
 args = parser.parse_args()
@@ -44,11 +45,11 @@ def getClusteringJointDistnFromCellInfo(signal_final_cell_info):
     assert isclose(joint_dist_rv['joint_prob'].sum(), 1), dt.datetime.now().isoformat() + ' ERROR: ' + ' Joint distribution does not sum to 1!'
     return joint_dist_rv
 
-def buildClusteringCompMeasureDict(mouse_name, bin_width, npy_dir, correction):
+def buildClusteringCompMeasureDict(mouse_name, bin_width, npy_dir, correction, correlation_type):
     """
     For building the dictionary with clustering comparison measures for a given mouse and bin width.
     """
-    signal_final_cell_info = ep.loadCommunityInfo(mouse_name, bin_width, npy_dir, correction=correction)
+    signal_final_cell_info = ep.loadCommunityInfo(mouse_name, bin_width, npy_dir, correction=correction, correlation_type=correlation_type)
     signal_final_cell_info['regional_cluster'] = [list(ep.regions).index(r) for r in signal_final_cell_info['cell_region']] # replacing region strings with integers for the metric functions
     region_clustering_rv = signal_final_cell_info['cell_region'].value_counts(normalize=True)
     consensus_clustering_rv = signal_final_cell_info['consensus_cluster'].value_counts(normalize=True)
@@ -61,9 +62,9 @@ def buildClusteringCompMeasureDict(mouse_name, bin_width, npy_dir, correction):
     norm_vi = 1 - (mi/joint_entropy) if joint_entropy != 0 else 0
     norm_id = 1 - norm_mi # information distance
     adj_rand_ind = adjusted_rand_score(signal_final_cell_info['regional_cluster'], signal_final_cell_info['consensus_cluster'])
-    return {'bin_width':bin_width, 'mouse_name':mouse_name, 'num_communities':signal_final_cell_info.consensus_cluster.max()+1, 'regional_clustering_entropy':entropy(region_clustering_rv.values), 'consensus_clustering_entropy':entropy(consensus_clustering_rv.values), 'joint_entropy':joint_entropy, 'mutual_information':mi, 'adjusted_mutual_information':adj_mi, 'normalised_mutual_information':norm_mi, 'variation_of_information':vi, 'normalised_variation_of_information':norm_vi, 'normalised_information_distance':norm_id, 'adjusted_rand_index':adj_rand_ind}
+    return {'bin_width':bin_width, 'mouse_name':mouse_name, 'correction':correction, 'correlation_type':correlation_type, 'num_communities':signal_final_cell_info.consensus_cluster.max()+1, 'regional_clustering_entropy':entropy(region_clustering_rv.values), 'consensus_clustering_entropy':entropy(consensus_clustering_rv.values), 'joint_entropy':joint_entropy, 'mutual_information':mi, 'adjusted_mutual_information':adj_mi, 'normalised_mutual_information':norm_mi, 'variation_of_information':vi, 'normalised_variation_of_information':norm_vi, 'normalised_information_distance':norm_id, 'adjusted_rand_index':adj_rand_ind}
 
-def plotClusteringCompMeasure(cluster_comp_frame, measure, correction):
+def plotClusteringCompMeasure(cluster_comp_frame, measure, correction, correlation_type):
     measure_to_ylabel = {'num_communities':'Num. Communities', 'regional_clustering_entropy':r'Regional clustering $H$ (bits)', 'consensus_clustering_entropy':r'Consensus clustering $H$ (bits)', 'joint_entropy':r'Joint $H$ (bits)', 'mutual_information':'Mutual Info. (bits)', 'adjusted_mutual_information':'Adj. Mutual Info.', 'normalised_mutual_information':'Norm. Mutual Info.', 'variation_of_information':'Var. of Info. (bits)', 'normalised_variation_of_information':'Norm. Var. of Info.', 'normalised_information_distance':'Norm. Info. Distance', 'adjusted_rand_index':'Adj. Rand Index', 'max_modularity':'Modularity'}
     for mouse_name in ep.mouse_names:
         relevant_cluster_comp_frame = cluster_comp_frame.loc[cluster_comp_frame.mouse_name == mouse_name, ['bin_width', measure]]
@@ -72,25 +73,25 @@ def plotClusteringCompMeasure(cluster_comp_frame, measure, correction):
     plt.xlabel('Bin width (s)', fontsize='x-large')
     plt.ylabel(measure_to_ylabel[measure], fontsize='x-large')
     plt.tight_layout()
-    file_name = os.path.join(image_dir, 'clustering_comparison', measure + '_' + correction + '.png')
+    file_name = os.path.join(image_dir, 'clustering_comparison', measure + '_' + correction + '_' + correlation_type + '.png')
     plt.savefig(file_name)
     print(dt.datetime.now().isoformat() + ' INFO: ' + file_name + ' saved.')
     plt.close()
 
 if (not args.debug) & (__name__ == "__main__"):
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Starting main function...')
-    cluster_comp_frame = pd.DataFrame(columns=['bin_width', 'mouse_name', 'num_communities', 'regional_clustering_entropy', 'consensus_clustering_entropy', 'joint_entropy', 'mutual_information', 'adjusted_mutual_information', 'normalised_mutual_information', 'variation_of_information', 'normalised_variation_of_information', 'normalised_information_distance', 'adjusted_rand_index'])
+    cluster_comp_frame = pd.DataFrame(columns=['bin_width', 'mouse_name', 'correction', 'correlation_type', 'num_communities', 'regional_clustering_entropy', 'consensus_clustering_entropy', 'joint_entropy', 'mutual_information', 'adjusted_mutual_information', 'normalised_mutual_information', 'variation_of_information', 'normalised_variation_of_information', 'normalised_information_distance', 'adjusted_rand_index'])
     for bin_width in ep.selected_bin_widths:
         print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing bin width ' + str(bin_width) + '...')
         for mouse_name in ep.mouse_names:
             print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing mouse ' + mouse_name + '...')
-            measure_dict = buildClusteringCompMeasureDict(mouse_name, bin_width, npy_dir, args.correction)
+            measure_dict = buildClusteringCompMeasureDict(mouse_name, bin_width, npy_dir, args.correction, args.correlation_type)
             cluster_comp_frame = cluster_comp_frame.append(measure_dict, ignore_index=True)
-    file_name = os.path.join(csv_dir, 'clustering_comparison', 'clustering_comparison.csv')
+    file_name = os.path.join(csv_dir, 'clustering_comparison', 'clustering_comparison_' + args.correction + '_' + args.correlation_type + '.csv')
     cluster_comp_frame.to_csv(file_name, index=False)
     print(dt.datetime.now().isoformat() + ' INFO: ' + file_name + ' saved.')
     for measure in cluster_comp_frame.columns[2:]:
         plotClusteringCompMeasure(cluster_comp_frame, measure, args.correction)
-    plotClusteringCompMeasure(pd.read_csv(os.path.join(csv_dir, 'community_detection_summary', 'community_detection_summary_' + args.correction + '.csv')), 'max_modularity', args.correction)
+    plotClusteringCompMeasure(pd.read_csv(os.path.join(csv_dir, 'community_detection_summary', 'community_detection_summary_' + args.correction + '_' + args.correlation_type + '.csv')), 'max_modularity', args.correction)
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Done.')
 
